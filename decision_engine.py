17040e9692924a57cac1b122d940afe7c29b6e01
Anubhav Sharma <anbv.contact@gmail.com>
2026-06-18 00:20:43 +0530

    refactor: move Gemini logic into decision_engine.py

---
+++ decision_engine.py
@@ -0,0 +1,21 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# user_input = input("Ask Anything that's troubling you...\n> ")
+
+response_instructions = "You are a decision-making assistant, you help people to come out of complex situations by providing them with solutions responding with pros, cons, blindspots user might be missing and an advice. Help the user handle the situation optimally."
+
+def get_response(question):
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = question,
+        config = genai.types.GenerateContentConfig(
+            system_instruction = response_instructions,
+            response_mime_type = "application/json")
+    )
+
+    return response.text
