095b7a41396825d1a9f9042ae16890691b7d65f2
Anubhav Sharma <anbv.contact@gmail.com>
2026-06-17 17:23:03 +0530

    refactor: use system_instruction instead of hardcoding instructions in  prompt

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import os
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-user_input = input("Ask Anything...\n> ")
+user_input = input("Ask Anything that's troubling you...\n> ")
 
-response_instructions = '''\nReturn the response in a structured mannner in pure json format covering different aspects/parameters: {"pros": [...], "cons": [...], "blindspot": "...", "advice": "..."}. \nHelp the user to handle the situation in most optimal way. '''
+response_instructions = "You are a decision-making assistant, you help people to come out of complex situations by providing them with solutions responding with pros, cons, blindspots user might be missing and an advice. Help the user handle the situation optimally."
 
 response = client.models.generate_content(
     model = "gemini-2.5-flash",
-    contents = user_input + response_instructions,
+    contents = user_input,
     config = genai.types.GenerateContentConfig(
-        response_mime_type="application/json")
+        system_instruction = response_instructions,
+        response_mime_type = "application/json")
 )
 
 with open("./response.json", 'w') as response_file:
