544fc809a778959847134df5471f150274a518b9
Anubhav Sharma <anbv.contact@gmail.com>
2026-06-18 15:59:53 +0530

    feat: define response_schema for structured JSON output

--- decision_engine.py
+++ decision_engine.py
@@ -1,21 +1,30 @@
-from google import genai
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from google import genai
+from pydantic import BaseModel
 import json
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+class ResponseFormat(BaseModel):
+    pros: list[str]
+    cons: list[str]
+    blind_spot: str
+    advice: str
 
 response_instructions = "You are a decision-making assistant/ strategist, you help people to come out of complex situations by providing them with solutions responding with pros, cons, blindspots user might be missing and advice. Help the user handle the situation optimally, make something out of their messy thoughts."
 
 
 def get_response(question):
     response = client.models.generate_content(
-        model = "gemini-2.5-flash",
+        # model = "gemini-2.5-flash",
+        model = "gemini-3.1-flash-lite",
         contents = question,
         config = genai.types.GenerateContentConfig(
             system_instruction = response_instructions,
-            response_mime_type = "application/json")
+            response_mime_type = "application/json",
+            response_schema=ResponseFormat)
     )
 
     return json.loads(response.text)
