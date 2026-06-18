0ffaaa5676980da6217227828044fb1cfb6e4822
Anubhav Sharma <anbv.contact@gmail.com>
2026-06-18 15:48:04 +0530

    refactor: decision_engine return json object instead of plain str

--- decision_engine.py
+++ decision_engine.py
@@ -1,13 +1,13 @@
 from google import genai
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# user_input = input("Ask Anything that's troubling you...\n> ")
+response_instructions = "You are a decision-making assistant/ strategist, you help people to come out of complex situations by providing them with solutions responding with pros, cons, blindspots user might be missing and advice. Help the user handle the situation optimally, make something out of their messy thoughts."
 
-response_instructions = "You are a decision-making assistant, you help people to come out of complex situations by providing them with solutions responding with pros, cons, blindspots user might be missing and an advice. Help the user handle the situation optimally."
 
 def get_response(question):
     response = client.models.generate_content(
@@ -18,4 +18,4 @@ def get_response(question):
             response_mime_type = "application/json")
     )
 
-    return response.text
+    return json.loads(response.text)
